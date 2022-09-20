090787088a226b9cf118b338b18d606e6fb6eaee
Matheus Marques Elias Correa <matheus123.ferrari.eng@gmail.com>
2022-09-20 20:42:21 -0300

    Getter,Setter e Construtor

---
+++ aula 04/src/aula/pkg04/Aula04.java
@@ -0,0 +1,11 @@
+
+package aula.pkg04;
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Amarela", 0.4f);
+        c1.status();
+        Caneta c2 = new Caneta ("kkk", "laranja", 0.5f);
+   
+    }
+    
+}
